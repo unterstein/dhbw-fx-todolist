68a13b9877eb27b2b41ada3da0656bac913ffdbc
Johannes Unterstein <unterstein@me.com>
2016-06-22 14:01:24 +0200

    switch scene

--- src/ba/java/todolist/view/login/LoginController.java
+++ src/ba/java/todolist/view/login/LoginController.java
@@ -1,4 +1,33 @@
 package ba.java.todolist.view.login;
 
-public class LoginController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+  @FXML
+  private Button button;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    button.setOnAction(e -> {
+      Stage stage = (Stage) button.getScene().getWindow();
+      try {
+        Parent root = FXMLLoader.load(getClass().getResource("listView.fxml"));
+        stage.setScene(new Scene(root, 300, 275));
+      } catch (IOException e1) {
+        e1.printStackTrace();
+      }
+    });
+  }
 }
